191ac20da900b6f6f166207e4070e11f7bdf5a82
Ummugulsum Cekin <ummugulsumerdim@gmail.com>
2022-10-07 16:48:31 +0300

    note

---
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Ummugulsum Çekin on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
